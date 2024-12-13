1625126499236978abc1a1de3a6787685b42b5ef
kongrui.1209 <kongrui.1209@bytedance.com>
2024-12-13 13:38:36 +0800

    update script config

--- scripts/eval_mixtral.sh
+++ scripts/eval_mixtral.sh
@@ -1,6 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+cd eagle/
 python3 -m eagle.evaluation.gen_ea_alpha_llama2chat \
-		 --ea-model-path /opt/tiger/mariana/EAGLE/EAGLE_files/hub/EAGLE-mixtral-instruct-8x7B \
+		 --ea-model-path /opt/tiger/mariana/EAGLE/EAGLE_files/output/for_debug \
 		 --base-model-path /opt/tiger/mariana/EAGLE/EAGLE_files/hub/Mixtral-8x7B-Instruct-v0.1
 
 # python3 -m eagle.evaluation.alpha
--- scripts/train_mixtral.sh
+++ scripts/train_mixtral.sh
@@ -1,5 +1,6 @@
-cd eagle/ge_data 
-python3 allocation_mixtral.py --outdir ../../data
+# cd eagle/ge_data 
+# python3 allocation_mixtral.py --outdir ../../data
+# cd ../../
 
-cd ../../
-accelerate launch -m --main_process_port 1234 --mixed_precision bf16 eagle.train.main --basepath /opt/tiger/mariana/EAGLE/EAGLE_files/hub/Mixtral-8x7B-Instruct-v0.1 --configpath /opt/tiger/mariana/EAGLE/eagle/train/Mixtral_8x7B_config.json --tmpdir data/  --cpdir output/
+
+accelerate launch -m --main_process_port 1234 --mixed_precision bf16 eagle.train.main --basepath /opt/tiger/mariana/EAGLE/EAGLE_files/hub/Mixtral-8x7B-Instruct-v0.1 --configpath /opt/tiger/mariana/EAGLE/eagle/train/Mixtral_8x7B_config.json --tmpdir data/  --cpdir EAGLE_files/output
