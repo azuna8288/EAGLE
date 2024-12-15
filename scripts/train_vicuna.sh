90c85735d9a43aba670219b5de05a5e6b1f69081
kongrui.1209 <kongrui.1209@bytedance.com>
2024-12-15 20:17:54 +0800

    update

--- scripts/train_vicuna.sh
+++ scripts/train_vicuna.sh
@@ -4,5 +4,5 @@
 accelerate launch -m --main_process_port 1234  --mixed_precision=bf16 eagle.train.main \
     --tmpdir data/ \
     --basepath hub/vicuna-7b-v1.3/ \
-    --configpath draft_configs/vicuna_7B_config_1layer.json \
+    --configpath draft_configs/vicuna_7B_config_1layer_feature_split.json \
     --cpdir output/ 
