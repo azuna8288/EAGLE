8b94fde7c24ed2fa064abbc57fb4095a58d98afc
kongrui.1209 <kongrui.1209@bytedance.com>
2024-12-15 10:14:37 +0800

    update log

--- eagle/train/main.py
+++ eagle/train/main.py
@@ -380,7 +380,7 @@ for epoch in range(num_epochs + 1):
         epoch_loss += loss.item()
         num_batches += 1
 
-        if batch_idx % 100 == 0:
+        if accelerator.is_local_main_process and batch_idx % 100 == 0:
             print('Train Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, num_epochs, epoch_loss))
 
     epoch_loss /= num_batches
