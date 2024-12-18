782d2aab9387ac0d6c63feb0f2254185971c9085
kongrui.1209 <kongrui.1209@bytedance.com>
2024-12-18 18:02:57 +0800

    update

--- setup.sh
+++ setup.sh
@@ -5,3 +5,12 @@ pip3 install modelscope
 pip3 install datasets
 pip3 install accelerate==0.26.0
 pip3 install -U pyarrow
+
+
+mkdir hub/
+mkdir output/
+mkdir data/
+
+cd hub/
+hdfs dfs -get hdfs://haruna/home/byte_data_seed/ssd_lq/public/seed_models/3b3_moe_p6/
+cd ..
