aab22d4e3fed63910d18870bf0b1b5b43e3c316e
kongrui.1209 <kongrui.1209@bytedance.com>
2024-12-18 16:41:49 +0800

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+pip3 install bytedance.seed_tokenizer
+pip3 install -e .
+pip3 install byted-seed-models==1.0.0
+pip3 install modelscope
+pip3 install datasets
+pip3 install accelerate==0.26.0
+pip3 install -U pyarrow
